e541d3e032f00af8be995e7ebf3fe3b629fcfb8b
Ayden Chance <achance6@jhu.edu>
2023-09-29 13:41:48 -0400

    Solved Problem 3

---
+++ EP3/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		final int PRIME_GEN_LIMIT = 100_000;
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Input number to compute prime factorization: ");
+		long result = scnr.nextLong();
+		ArrayList<Integer> factors = new ArrayList<>();
+		ArrayList<Integer> primeNumbers = generatePrimes(PRIME_GEN_LIMIT);
+		while (result != 1) {
+			long tmp = result;
+			for (int prime : primeNumbers) {
+				if (result % prime == 0) {
+					factors.add(prime);
+					result = result / prime;
+					// System.out.println(prime + " " + tmp);
+					break;
+				}
+			}
+			if (tmp == result) break;
+		}
+		System.out.println(factors);
+		scnr.close();
+	}
+	
+	// Sieve of Eratosthenes
+	static ArrayList<Integer> generatePrimes(int n) {
+		Boolean[] ints = new Boolean[n];
+		int p = 2; //smallest prime number
+		ints[0] = true; ints[1] = true;
+		for (int i = 2; i < n; ++i) {
+			ints[i] = false;
+		}
+		
+		boolean flg = true;
+		while (flg) {
+			for (int i = 2 * p; i < n; i = i + p) {
+				ints[i] = true;
+			}
+			int tmp = p;
+			for (int i = p + 1; i < n; ++i) {
+				if (!ints[i]) {
+					p = i;
+					break;
+				}
+			}
+			if (tmp == p) flg = false;
+		}
+		ArrayList<Integer> primes = new ArrayList<>();
+		for (int i = 0; i < n; ++i) {
+			if (!ints[i]) primes.add(i);
+		}
+		return primes;
+	}
+}
