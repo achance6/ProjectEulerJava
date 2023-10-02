25a67f1cb962cf296e7cf53f253a6a9b671d23e4
Ayden Chance <achance6@jhu.edu>
2023-10-02 14:57:22 -0400

    Solved problem 5

--- EP5/src/Main.java
+++ EP5/src/Main.java
@@ -1,9 +1,18 @@
-
+/**
+ * What is the smallest positive number that is evenly divisible 
+ * by all of the numbers from 1 to 20?
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int sum = 0, cur = 0;
+		while (sum != 20) {
+			cur++;
+			sum = 0;
+			for (int i = 1; i < 21; ++i) {
+				if (cur % i == 0) sum++;
+			}
+		}
+		System.out.println(cur);
 	}
-
 }
