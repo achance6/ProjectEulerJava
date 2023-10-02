a3783b37b825a7bbd66391fb47e35981997e37a4
Ayden Chance <achance6@jhu.edu>
2023-10-02 11:26:33 -0400

    Created isPalindrome function

--- EP4/src/Main.java
+++ EP4/src/Main.java
@@ -1,11 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Problem: Find the largest palindrome made from the product of two 3-digit numbers.
  */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scnr = new Scanner(System.in);
+		int num = scnr.nextInt();
+		System.out.println(isPalindrome(num));
+		
+	}
+	
+	public static boolean isPalindrome(int num) {
+		String n = ((Integer) num).toString();
+		ArrayList<Character> firstHalf = new ArrayList<>();
+		ArrayList<Character> secondHalf = new ArrayList<>();
+		for (int i = 0; i < n.length() / 2; ++i) {
+			firstHalf.add(n.charAt(i));
+			secondHalf.add(n.charAt(n.length() - 1 - i));
+		}
+		if (firstHalf.equals(secondHalf)) return true;
+		return false;
 	}
 
 }
