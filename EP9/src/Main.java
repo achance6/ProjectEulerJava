69a8f57dba8d5c5c94e4f642d66d33c5034b2e9e
Ayden Chance <achance6@jhu.edu>
2023-10-03 15:17:41 -0400

    Solved Problem 9

--- EP9/src/Main.java
+++ EP9/src/Main.java
@@ -1,8 +1,23 @@
 
+
+/**
+ * Problem: There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final int LIMIT = 1000;
+		
+		for (int a = 1; a < LIMIT; ++a) {
+			for (int b = 1; b < LIMIT; ++b) {
+				for (int c = 1; c < LIMIT; ++c) {
+					if (a + b + c == LIMIT && (a * a) + (b * b) == (c * c)) {
+						System.out.println(a * b * c);
+					}
+				}
+			}
+		}
 
 	}
 
