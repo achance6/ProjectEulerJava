ed21daaa7e6636ab29a6a34ba35905d3fae9712a
Ayden Chance <achance6@jhu.edu>
2023-10-07 13:45:11 -0400

    Added Problem 12

---
+++ EP12/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Problem: What is the value of the first triangle number 
+ * to have over five hundred divisors?
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
