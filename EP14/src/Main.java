4b6373847ecb81392031ec22e6356f189bd6a69b
Ayden Chance <achance6@jhu.edu>
2023-10-10 10:04:56 -0400

    Solved problem 14

--- EP14/src/Main.java
+++ EP14/src/Main.java
@@ -10,29 +10,36 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> seq = genCollatzSequence(900000);
 		int largestSize = 0;
 		int numWithLargestSeq = 1;
-//		for (int i = 1; i < 1_000_000; ++i) {
-//			if (genCollatzSequence(i).size() > largestSize) numWithLargestSeq = i;
-//		}
-//		System.out.println(numWithLargestSeq);
+		for (int i = 1; i < 1_000_000; ++i) {
+			int curSize = genCollatzSequence(i).size();
+			if (curSize > largestSize) {
+				largestSize = curSize;
+				numWithLargestSeq = i;
+			}
+		}
+		System.out.println(numWithLargestSeq);
 
 	}
 	
-	private static ArrayList<Integer> genCollatzSequence(int n) {
-		ArrayList<Integer> seq = new ArrayList<>();
+	private static ArrayList<Long> genCollatzSequence(long n) {
+		ArrayList<Long> seq = new ArrayList<>();
 		seq.add(n);
-		int res = n;
-		while (res != 1) {
-			if (res % 2 == 0) {
-				res = res / 2;
-				seq.add(res);
+		long res = n;
+		try {
+			while (res != 1) {
+				if (res % 2 == 0) {
+					res = res / 2;
+					seq.add(res);
+				}
+				else {
+					res = (3 * res) + 1;
+					seq.add(res);
+				}
 			}
-			else {
-				res = (3 * res) + 1;
-				seq.add(res);
-			}
+		} catch (Exception e) {
+			System.out.println("out of bounds");
 		}
 		return seq;
 	}
