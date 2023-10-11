d438ac799ede923766c6fa5b34a8a4f7ed517ca9
Ayden Chance <achance6@jhu.edu>
2023-10-11 15:47:46 -0400

    added problem 17

---
+++ EP17/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Problem: If all the numbers from 1 to 1000 (one thousand) inclusive 
+ * were written out in words, how many letters would be used? 
+ * Do not count spaces and hyphens.
+ * Examples: 342 = three hundred and forty-two
+ * 115 = one hundred and fifteen
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
