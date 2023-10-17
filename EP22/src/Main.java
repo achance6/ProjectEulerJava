e15a1eee2c3de22cc1bf8dd0d3cda1b1e05a7e75
Ayden Chance <achance6@jhu.edu>
2023-10-17 17:20:50 -0400

    Added problem 22

---
+++ EP22/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Problem:
+ * Using names.txt (right click and 'Save Link/Target As...'), 
+ * a 46K text file containing over five-thousand first names, 
+ * begin by sorting it into alphabetical order. 
+ * Then working out the alphabetical value for each name, 
+ * multiply this value by its alphabetical position in the list to obtain a name score.
+ * 
+ * What is the total of all name scores in the file?
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
