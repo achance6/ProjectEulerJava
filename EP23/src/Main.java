d73b43d2522f5475e24c4fa77e22174209c5a367
Ayden Chance <achance6@jhu.edu>
2023-10-18 13:36:23 -0400

    Abundant numbers generated

--- EP23/src/Main.java
+++ EP23/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Problem: Find the sum of all the positive integers 
  * which cannot be written as the sum of two abundant numbers.
@@ -5,11 +7,51 @@
  */
 public class Main {
 
+	private static final int LIM = 28123;
 	public static void main(String[] args) {
+		ArrayList<Integer> abundantNums = new ArrayList<>();
+		for (int i = 0; i <= LIM; ++i) {
+			if (isAbundant(i)) abundantNums.add(i);
+		}
+		long sum = 0;
+		System.out.println(abundantNums);
+//		for (int i = 0; i <= LIM; ++i) {
+//			boolean isSum = false;
+//			for (int j : abundantNums) {
+//				for (int k : abundantNums) {
+//					if (i == j + k) {
+//						isSum = true;
+//						break;
+//					}
+//				}
+//			}
+//			if (! isSum) {
+//				sum += i;
+//				System.out.println(i);
+//			}
+//		}
+		System.out.println(sum);
 		
-
 	}
 	
+	private static ArrayList<Long> genProperDivisors(long num) {
+		ArrayList<Long> factors = new ArrayList<>();
+		for (long i = 1; i < num; ++i) {
+			if (num % i == 0) factors.add(i);
+		}
+		return factors;
+	}
 	
+	private static long sumProperDivisors(long num) {
+		long sum = 0;
+		for (long i = 1; i < num; ++i) {
+			if (num % i == 0) sum += i;
+		}
+		return sum;
+	}
+	
+	private static boolean isAbundant(long num) {
+		return sumProperDivisors(num) > num;
+	}
 
 }
