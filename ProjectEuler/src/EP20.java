d03289dca97c9689794dc482ddec045961853cf7
Ayden Chance <achance6@jhu.edu>
2023-10-18 17:48:54 -0400

    Ported problem 20

--- ProjectEuler/src/EP20.java
+++ ProjectEuler/src/EP20.java
@@ -1,10 +1,17 @@
+import java.math.BigInteger;
 
+/**
+ * Problem: Find the sum of the digits in the number 100!.
+ */
 public class EP20 implements EulerSolution {
 
 	@Override
 	public String run() {
-		// TODO Auto-generated method stub
-		return null;
+		BigInteger big = Methods.fact(new BigInteger("100"));
+		char[] charNums = big.toString().toCharArray();
+		int sum = 0;
+		for(char c : charNums) sum += Character.getNumericValue(c);
+		return Integer.toString(sum);
 	}
 
 }
--- ProjectEuler/src/Main.java
+++ ProjectEuler/src/Main.java
@@ -20,7 +20,7 @@ public final class Main {
 		System.out.println((new EP17()).run());
 		System.out.println((new EP18()).run());
 		System.out.println((new EP19()).run());
-//		System.out.println((new EP20()).run());
+		System.out.println((new EP20()).run());
 //		System.out.println((new EP21()).run());
 //		System.out.println((new EP22()).run());
 //		System.out.println((new EP23()).run());
