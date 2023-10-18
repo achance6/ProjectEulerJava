ba9f5bd605be8a83037860a88b67a918060e0769
Ayden Chance <achance6@jhu.edu>
2023-10-18 17:40:26 -0400

    ported problem 15

--- ProjectEuler/src/EP15.java
+++ ProjectEuler/src/EP15.java
@@ -1,10 +1,14 @@
+import java.math.BigInteger;
 
+/**
+ * Problem: How many lattice paths are there through a 20x20 grid?
+ */
 public class EP15 implements EulerSolution {
 
 	@Override
 	public String run() {
-		// TODO Auto-generated method stub
-		return null;
+		// 40 choose 20
+		return Methods.binCo(new BigInteger("40"), new BigInteger("20")).toString();
 	}
 
 }
