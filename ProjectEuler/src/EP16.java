1eaa8a18bfe00d2978a0745657afbb3c5d737c1d
Ayden Chance <achance6@jhu.edu>
2023-10-18 17:41:48 -0400

    Ported problem 16

--- ProjectEuler/src/EP16.java
+++ ProjectEuler/src/EP16.java
@@ -1,10 +1,18 @@
+import java.math.BigInteger;
 
+/**
+ * Problem: What is the sum of the digits of the number 2^1000?
+ */
 public class EP16 implements EulerSolution {
 
 	@Override
 	public String run() {
-		// TODO Auto-generated method stub
-		return null;
+		BigInteger num = (new BigInteger("2")).pow(1000);
+		char[] s = num.toString().toCharArray();
+		int sum = 0;
+		for (int i = 0; i < s.length; ++i) 
+			sum += Character.getNumericValue(s[i]);
+		return Integer.toString(sum);
 	}
 
 }
