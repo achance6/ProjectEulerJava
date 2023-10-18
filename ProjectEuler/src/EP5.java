9f038c7aff89a2cca0f1dca3e22e4dcbb234619b
Ayden Chance <achance6@jhu.edu>
2023-10-18 16:37:34 -0400

    Ported and optimized problem 5

--- ProjectEuler/src/EP5.java
+++ ProjectEuler/src/EP5.java
@@ -1,10 +1,20 @@
-
+/**
+ * Problem: What is the smallest positive number that is evenly divisible 
+ * by all of the numbers from 1 to 20?
+ */
 public class EP5 implements EulerSolution {
 
 	@Override
 	public String run() {
-		// TODO Auto-generated method stub
-		return null;
+		int sum = 0, cur = 0;
+		while (sum != 20) {
+			cur++;
+			sum = 0;
+			for (int i = 1; i < 21; ++i) {
+				if (cur % i != 0) break; // optimization
+				else sum++;
+			}
+		}
+		return Integer.toString(cur);
 	}
-
 }
--- ProjectEuler/src/Main.java
+++ ProjectEuler/src/Main.java
@@ -5,7 +5,7 @@ public final class Main {
 		System.out.println((new EP2()).run()); 
 		System.out.println((new EP3()).run());
 		System.out.println((new EP4()).run());
-//		System.out.println((new EP5()).run());
+		System.out.println((new EP5()).run());
 //		System.out.println((new EP6()).run());
 //		System.out.println((new EP7()).run());
 //		System.out.println((new EP8()).run());
