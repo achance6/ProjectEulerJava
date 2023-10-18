929d769f86f0de2287fa7ab72422699d306ecb11
Ayden Chance <achance6@jhu.edu>
2023-10-18 13:19:14 -0400

    Added problem 23

---
+++ EP23/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Problem: Find the sum of all the positive integers 
+ * which cannot be written as the sum of two abundant numbers.
+ * all integers greater than 28123 can be written as the sum of two abundant numbers.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	
+
+}
