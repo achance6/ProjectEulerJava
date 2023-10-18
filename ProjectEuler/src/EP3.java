ea3acc565bf5b90802fc1d3b218a54d1131dfc07
Ayden Chance <achance6@jhu.edu>
2023-10-18 16:29:20 -0400

    minor readability edit problem 3

--- ProjectEuler/src/EP3.java
+++ ProjectEuler/src/EP3.java
@@ -10,8 +10,8 @@ public class EP3 implements EulerSolution {
 	public String run() {
 		long result = Long.parseLong("600851475143");
 		ArrayList<Integer> factors = Methods.genPrimeFactors(result, PRIME_GEN_LIMIT);
-		factors.sort(null);
-		return Integer.toString(factors.get(factors.size() - 1));
+		factors.sort((a, b) -> b - a); //reverse natural order (descending)
+		return Integer.toString(factors.get(0));
 	}
 
 }
