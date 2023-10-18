25492c4ff215986fe8a418cff91db91dea558589
Ayden Chance <achance6@jhu.edu>
2023-10-18 15:39:41 -0400

    refactored problem 2 to get method from it

--- ProjectEuler/src/EP2.java
+++ ProjectEuler/src/EP2.java
@@ -7,16 +7,10 @@ public class EP2 implements EulerSolution {
 	
 	@Override
 	public String run() {
-		int last_1 = 2;
-		int last_2 = 1;
-		int sum = 0;
-		do {
-			if (last_1 % 2 == 0) sum += last_1;
-			int tmp = last_1;
-			last_1 = last_1 + last_2;
-			last_2 = tmp;
-		} while (last_1 < LIMIT);
-		return Integer.toString(sum);
+		long sum = 0;
+		for (Long c : Methods.genFibSeq(LIMIT)) {
+			if (c % 2 == 0) sum += c;
+		}
+		return Long.toString(sum);
 	}
-
 }
--- ProjectEuler/src/Methods.java
+++ ProjectEuler/src/Methods.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 
 public final class Methods {
-
+	public static ArrayList<Long> genFibSeq(final long LIM) {
+		ArrayList<Long> seq = new ArrayList<>();
+		long last_1 = 2;
+		long last_2 = 1;
+		do {
+			seq.add(last_1);
+			long tmp = last_1;
+			last_1 = last_1 + last_2;
+			last_2 = tmp;
+		} while (last_1 <= LIM);
+		return seq;
+	}
 }
