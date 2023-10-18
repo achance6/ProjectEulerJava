936c9c071a100176d8a310af687cb07313381070
Ayden Chance <achance6@jhu.edu>
2023-10-18 17:36:57 -0400

    Ported problem 13

--- ProjectEuler/src/Main.java
+++ ProjectEuler/src/Main.java
@@ -13,7 +13,7 @@ public final class Main {
 		System.out.println((new EP10()).run());
 		System.out.println((new EP11()).run());
 		//System.out.println((new EP12()).run()); // Takes really long
-//		System.out.println((new EP13()).run());
+		System.out.println((new EP13()).run());
 //		System.out.println((new EP14()).run());
 //		System.out.println((new EP15()).run());
 //		System.out.println((new EP16()).run());
