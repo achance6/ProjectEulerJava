3da91ccaabc025c20b13a7cc399792420ddb74da
Ayden Chance <achance6@jhu.edu>
2023-10-19 16:02:10 -0400

    Removed static modifier from all tests

--- src/SolutionTester.java
+++ src/SolutionTester.java
@@ -4,50 +4,50 @@ import org.junit.jupiter.api.Test;
 public final class SolutionTester {
 
 	@Test
-	public static void testEP01() { assertEquals("233168", (new EP01()).run()); }
+	public void testEP01() { assertEquals("233168", (new EP01()).run()); }
 	@Test
-	public static void testEP02() { assertEquals("4613732", (new EP02()).run()); }
+	public void testEP02() { assertEquals("4613732", (new EP02()).run()); }
 	@Test
-	public static void testEP03() { assertEquals("6857", (new EP03()).run()); }
+	public void testEP03() { assertEquals("6857", (new EP03()).run()); }
 	@Test
-	public static void testEP04() { assertEquals("906609", (new EP04()).run()); }
+	public void testEP04() { assertEquals("906609", (new EP04()).run()); }
 	@Test
-	public static void testEP05() { assertEquals("232792560", (new EP05()).run()); }
+	public void testEP05() { assertEquals("232792560", (new EP05()).run()); }
 	@Test
-	public static void testEP06() { assertEquals("25164150", (new EP06()).run()); }
+	public void testEP06() { assertEquals("25164150", (new EP06()).run()); }
 	@Test
-	public static void testEP07() { assertEquals("104743", (new EP07()).run()); }
+	public void testEP07() { assertEquals("104743", (new EP07()).run()); }
 	@Test
-	public static void testEP08() { assertEquals("23514624000", (new EP08()).run()); }
+	public void testEP08() { assertEquals("23514624000", (new EP08()).run()); }
 	@Test
-	public static void testEP09() { assertEquals("31875000", (new EP09()).run()); }
+	public void testEP09() { assertEquals("31875000", (new EP09()).run()); }
 	@Test
-	public static void testEP10() { assertEquals("142913828922", (new EP10()).run()); }
+	public void testEP10() { assertEquals("142913828922", (new EP10()).run()); }
 	@Test
-	public static void testEP11() { assertEquals("70600674", (new EP11()).run()); }
+	public void testEP11() { assertEquals("70600674", (new EP11()).run()); }
 	@Test
-	public static void testEP12() { assertEquals("76576500", (new EP12()).run()); }
+	public void testEP12() { assertEquals("76576500", (new EP12()).run()); }
 	@Test
-	public static void testEP13() { assertEquals("5537376230", (new EP13()).run()); }
+	public void testEP13() { assertEquals("5537376230", (new EP13()).run()); }
 	@Test
-	public static void testEP14() { assertEquals("837799", (new EP14()).run()); }
+	public void testEP14() { assertEquals("837799", (new EP14()).run()); }
 	@Test
-	public static void testEP15() { assertEquals("137846528820", (new EP15()).run()); }
+	public void testEP15() { assertEquals("137846528820", (new EP15()).run()); }
 	@Test
-	public static void testEP16() { assertEquals("1366", (new EP16()).run()); }
+	public void testEP16() { assertEquals("1366", (new EP16()).run()); }
 	@Test
-	public static void testEP17() { assertEquals("21124", (new EP17()).run()); }
+	public void testEP17() { assertEquals("21124", (new EP17()).run()); }
 	@Test
-	public static void testEP18() { assertEquals("1074", (new EP18()).run()); }
+	public void testEP18() { assertEquals("1074", (new EP18()).run()); }
 	@Test
-	public static void testEP19() { assertEquals("171", (new EP19()).run()); }
+	public void testEP19() { assertEquals("171", (new EP19()).run()); }
 	@Test
-	public static void testEP20() { assertEquals("648", (new EP20()).run()); }
+	public void testEP20() { assertEquals("648", (new EP20()).run()); }
 	@Test
-	public static void testEP21() { assertEquals("31626", (new EP21()).run()); }
+	public void testEP21() { assertEquals("31626", (new EP21()).run()); }
 	@Test
-	public static void testEP22() { assertEquals("871198282", (new EP22()).run()); }
+	public void testEP22() { assertEquals("871198282", (new EP22()).run()); }
 	@Test
-	public static void testEP23() { assertEquals("4179871", (new EP23()).run()); }
+	public void testEP23() { assertEquals("4179871", (new EP23()).run()); }
 	
 }
