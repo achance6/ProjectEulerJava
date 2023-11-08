e6dab8d51ea84c98b607b0fc65868e20bec862af
Ayden Chance <achance6@jhu.edu>
2023-11-08 14:31:44 -0500

    Fixed fibonacci method tests

--- src/MethodTester.java
+++ src/MethodTester.java
@@ -3,15 +3,15 @@ import org.junit.jupiter.api.Test;
 
 public final class MethodTester {
 
-//	@Test
-//	public void testGenFibSeq() { 
-//		assertEquals("[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]", 
-//			Methods.genFibSeq(1_000)); 
-//		}
-//	
-//	@Test
-//	public void testGenFibSeqString() { 
-//		assertEquals("[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]", 
-//			Methods.genFibSeq("1_000")); 
-//		}
+	@Test
+	public void testGenFibSeq() { 
+		assertEquals("[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]", 
+			Methods.genFibSeq(1_000).toString()); 
+		}
+	
+	@Test
+	public void testGenFibSeqString() { 
+		assertEquals("[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]", 
+			Methods.genFibSeq("1000").toString()); 
+		}
 }
